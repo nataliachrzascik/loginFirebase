18160680fe7a1ecceb62d3a780192e51d5f218c4
Natalia Chrząścik <56516256+nataliachrzascik@users.noreply.github.com>
2020-05-12 16:49:36 +0200

    Add files via upload

---
+++ precache-manifest.501a0f3e62dba25d641ad79ac4574728.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db5202b558eec1c25793e62a9b730638",
+    "url": "/loginFirebase/index.html"
+  },
+  {
+    "revision": "6ef3ac1565908b2db4e5",
+    "url": "/loginFirebase/static/js/2.282c9eb8.chunk.js"
+  },
+  {
+    "revision": "265068e7f3bc904803e029152d67e9a1",
+    "url": "/loginFirebase/static/js/2.282c9eb8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7f3fdd580ad23a11697",
+    "url": "/loginFirebase/static/js/main.e4436ab5.chunk.js"
+  },
+  {
+    "revision": "5d059eef4f0aa0398d6e",
+    "url": "/loginFirebase/static/js/runtime-main.c31c0ec8.js"
+  },
+  {
+    "revision": "ac6ad1140b8000138e8dab256ea4cf4e",
+    "url": "/loginFirebase/static/media/cat.ac6ad114.jpg"
+  },
+  {
+    "revision": "1ae5bc7bcc79ff704eee264d997b4579",
+    "url": "/loginFirebase/static/media/fire.1ae5bc7b.jpg"
+  },
+  {
+    "revision": "8629d601d474b4595fc0d805fa3de492",
+    "url": "/loginFirebase/static/media/fox.8629d601.jpg"
+  },
+  {
+    "revision": "b364ed5dcc8c0497f56190230b209c77",
+    "url": "/loginFirebase/static/media/girl.b364ed5d.jpg"
+  },
+  {
+    "revision": "a1b9f52b92cc92a92551302f51f5e0cb",
+    "url": "/loginFirebase/static/media/lego.a1b9f52b.jpg"
+  }
+]);
